34726a0b652f02653bcaa77e24d77dcf0fd38840
sayen <smgzg@proton.me>
2023-04-21 18:21:15 +0800

    album ver 0.0.1

---
+++ README.md
@@ -0,0 +1,5 @@
+This plugin is still in active development and may change frequently
+
+## Documentation
+
+The Discourse Album Plugin: For user quick search for their images
---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: album
+# about: album function for user to easy manage the images uploaded
+# version: 0.0.1
+# authors: Cool
+# url: local
+
+enabled_site_setting :album_enabled
