76cb8f24b32e3cec2bc2fb4735911c6e94451d70
Johnsie <johnsie@gmail.com>
2024-02-04 23:34:01 +0800

    ...

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-album
 # about: album function for user to easy manage the images uploaded
 # version: 0.0.1
-# authors: Cool
+# authors: JohnsieBorer
 # url: local
 
 enabled_site_setting :album_enabled
